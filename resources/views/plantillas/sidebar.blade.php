cb546f72588a30ee9d7bcc91682f714615411e7e
Jose Villarroel <oscarclarke9900@gmail.com>
2018-11-18 14:29:44 -0400

    some fixes

--- resources/views/plantillas/sidebar.blade.php
+++ resources/views/plantillas/sidebar.blade.php
@@ -13,9 +13,9 @@
                             <li class="nav-item">
                                 <a @click="menu=1" class="nav-link" href="#"><i class="icon-arrow-right-circle"></i> Ingresos</a>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a @click="menu=2" class="nav-link" href="#"><i class="icon-arrow-left-circle"></i> Salidas</a>
-                            </li>
+                            </li> -->
                         </ul>
                     </li>
                     <li class="nav-item nav-dropdown">
@@ -24,9 +24,9 @@
                             <li class="nav-item">
                                 <a @click="menu=3" class="nav-link" href="i#"><i class="icon-chart"></i> Reporte Celdas</a>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a @click="menu=4" class="nav-link" href="#"><i class="icon-chart"></i> Reporte Ventas</a>
-                            </li>
+                            </li> -->
                         </ul>
                     </li>
                 </ul>
